e07fd4c552d73580280e93d37ecd0d421ef0116d
Toni D <tonidy@outlook.com>
2025-01-06 20:15:07 +0700

    Update code

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,20 @@ export default defineConfig({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    ssr: {
+      // external: ["node:fs", "node:path", "node:url"],
+    },
+    build: {
+      rollupOptions: {
+        external: [
+          'unenv/runtime/node/timers/$cloudflare.mjs',
+          'unenv/runtime/node/console/$cloudflare.mjs',
+          'unenv/runtime/polyfill/performance.mjs',
+          'unenv/runtime/node/process/$cloudflare.mjs'
+        ]
+      }
+    }
+  }
 });
